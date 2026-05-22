c4a155b2c6c2d23d8a49459a78de18240e3f39ad
Paschal Imekaeze <mails2paschal@gmail.com>
2026-05-22 17:10:51 +0100

    feat: implement initial structure for fct_trips.sql with trip data selection

--- 04-analytics-engineering/taxi_rides_ny/models/marts/fct_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/marts/fct_trips.sql
@@ -1,1 +1,36 @@
-select 1 as dummy
+/*
+Todo 
+- One row per trip doesnt matter if yellow or Green
+- Add a primary key (trip_id). It has tobe unique
+- Find all the duplicates, understand why they happen and fix them 
+- Find a way to enrich the column payment_type.
+*/
+
+
+
+with trips_unioned as (
+    select * from {{ ref('int_trips_unioned') }}
+),
+trips as (
+    select
+        trip_id,
+        vendor_id,
+        pickup_datetime,
+        dropoff_datetime,
+        passenger_count,
+        trip_distance,
+        pickup_location_id,
+        dropoff_location_id,
+        payment_type,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount, 
+        tolls_amount,
+        improvement_surcharge,
+        total_amount
+    from trips_unioned
+)
+select * from trips
+
+
