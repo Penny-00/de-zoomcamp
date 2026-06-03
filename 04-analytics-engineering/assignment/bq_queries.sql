567fdb33687d86fabafe497b797957cd4e787d9c
Paschal Imekaeze <mails2paschal@gmail.com>
2026-06-03 08:41:40 +0100

    feat: add query for counting Green taxi trips in October 2019

--- 04-analytics-engineering/assignment/bq_queries.sql
+++ 04-analytics-engineering/assignment/bq_queries.sql
@@ -13,3 +13,12 @@ WHERE service_type = 'Green'
 GROUP BY pickup_zone
 ORDER BY total_revenue DESC
 LIMIT 1;
+
+
+-- Green Taxi Trip Counts (October 2019)
+SELECT
+  SUM(total_monthly_trips) AS total_trips
+FROM `kestra-sandbox-493016.dbt_penny_prod.fct_monthly_zone_revenue`
+WHERE service_type = 'Green'
+  AND EXTRACT(YEAR FROM revenue_month) = 2019
+  AND EXTRACT(MONTH FROM revenue_month) = 10;
